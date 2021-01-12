4b7c23b3bceb218fb503ca78ebef5406c86d51ce
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-01-12 14:12:10 +0100

    Avanzamento struttura Header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
-// webpack.mix.js
-
 let mix = require('laravel-mix');
-
-mix.sass('src/app.scss', 'css');
+mix.sass('src/app.scss', 'css').options({
+    processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
+});
