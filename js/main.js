c3f3616bd61ae4ceb7332bb65d0fabae383b7fb8
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-01-13 10:13:01 +0100

    Inizio parte vue + hover sui button + fix h2 numbers

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#root",
+  data: {
+    // array per header top
+    contactInformations: {},
+    // array per header bottom
+    nav: {},
+  },
+  methods: {
+
+  },
+});
