caf389828d6ef3413159d3726399b1655b4bf4aa
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-01-13 10:52:01 +0100

    Primo tentativo di stampare l'header top con vue

--- js/main.js
+++ js/main.js
@@ -2,9 +2,29 @@ var app = new Vue({
   el: "#root",
   data: {
     // array per header top
-    contactInformations: {},
+    contactInformations: [
+      {
+        icon: '<i class="fas fa-phone-alt"></i>',
+        text: "+1 (305) 1234-5678",
+      },
+      {
+        icon: '<i class="fas fa-envelope"></i>',
+        text: "hello@example.com",
+      },
+      '<i class="fab fa-facebook-f"></i>',
+      '<i class="fab fa-twitter"></i>',
+      '<i class="fab fa-linkedin-in"></i>',
+    ],
     // array per header bottom
-    nav: {},
+    nav: [
+      "home",
+      "about",
+      "services",
+      "team",
+      "blog",
+      '<i class="far fa-user"></i>',
+      '<button class="button_green">get in touch</button>'
+    ],
   },
   methods: {
 
